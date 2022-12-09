c345fe50b86fd0859ac25e89ccdec271a43c83bd
Job Alvarez <jobitoalv@gmail.com>
2022-12-09 11:00:08 +1000

    fixed html styling

--- weather.js
+++ weather.js
@@ -1,13 +1,12 @@
 class Weather {
-    constructor(city, state) {
+    constructor(city) {
       this.apiKey = 'b618cc59d6f901c18cdf81a45950d75d';
-      this.city = city;
-      this.state = state;
+      this.city = city.name;
     }
   
     // Fetch weather from API
     async getWeather() {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?city=${this.city}&state=${this.state}&appid=${this.apikey}.json`);
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}.json`);
   
       const responseData = await response.json();
   
@@ -15,8 +14,7 @@ class Weather {
     }
   
     // Change weather location
-    changeLocation(city, state) {
+    changeLocation(city) {
       this.city = city;
-      this.state = state;
     }
   }
