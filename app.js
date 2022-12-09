4d4d688b8e4921915ca83185ad4a081f38cf93ec
Job Alvarez <jobitoalv@gmail.com>
2022-12-09 10:30:19 +1000

    class UI ns paint added to ui.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', getWeather);
 function getWeather(){
   weather.getWeather()
     .then(results => {
-      console.log(results);
+        ui.paint(results);
     })
     .catch(err => console.log(err));
 }
