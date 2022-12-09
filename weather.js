d271b0c5359799fc6de34e311fcc6d3cca211f23
Job Alvarez <jobitoalv@gmail.com>
2022-12-09 10:23:26 +1000

    update api fetch link

--- weather.js
+++ weather.js
@@ -7,7 +7,7 @@ class Weather {
   
     // Fetch weather from API
     async getWeather() {
-      const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?city=${this.city}&state=${this.state}&appid=${this.apikey}.json`);
   
       const responseData = await response.json();
   
