ec7d84f738ef66d472d13a743685fc3a295a013e
Job Alvarez <jobitoalv@gmail.com>
2022-12-09 11:02:06 +1000

    updated the right api key

--- weather.js
+++ weather.js
@@ -1,20 +1,26 @@
 class Weather {
     constructor(city) {
       this.apiKey = 'b618cc59d6f901c18cdf81a45950d75d';
-      this.city = city.name;
-    }
-  
-    // Fetch weather from API
-    async getWeather() {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}.json`);
-  
-      const responseData = await response.json();
-  
-      return responseData.current_observation;
-    }
-  
-    // Change weather location
-    changeLocation(city) {
       this.city = city;
+      this.country = country;
     }
+  
+      // fetch weather from API
+  async getWeather() {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${
+        this.country
+      }&units=metric&APPID=${this.apiKey}`
+    );
+
+    const responseData = await response.json();
+
+    return responseData;
   }
+
+  // change weather location
+  changeLocation(city, country) {
+    this.city = city;
+    this.country = country;
+  }
+}
